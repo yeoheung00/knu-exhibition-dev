30d5d84b6ac4758230b30bb01b652e71d635a914
민경원 <min-kyeongwon@mingyeong-won-ui-noteubug.local>
2023-06-08 16:52:16 +0900

    nav

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,17 +1,34 @@
+'use client'
+
 import Link from "next/link";
+import styles from "./header.module.css";
+import { useState } from "react";
 
 export default function Header(){
+  const [isOpen, setIsOpen] = useState(false);
+  const open = () => {
+    setIsOpen((current) => !current);
+    console.log(isOpen);
+  }
+  const close = () => {
+    setIsOpen(false);
+  }
   return(
-    <div>
-      <h1>국립공주대학교 디자인컨버전스학과 졸업전시</h1>
-      <nav>
+    <div className={styles.root}>
+      <h1 className={styles.title}>Piece Piece Piece</h1>
+      <nav className={`${styles.nav} ${isOpen?styles.active:null}`}>
         <ul>
-          <li><Link href="/">Home</Link></li>
-          <li><Link href="/about">About</Link></li>
-          <li><Link href="/profile">Profile</Link></li>
-          <li><Link href="/gallery">Gallery</Link></li>
+          <li><Link onClick={close} href="/">Home</Link></li>
+          <li><Link onClick={close} href="/about">About</Link></li>
+          <li><Link onClick={close} href="/profile">Profile</Link></li>
+          <li><Link onClick={close} href="/gallery">Gallery</Link></li>
         </ul>
       </nav>
+      <div className={styles.opener} onClick={open}>
+        <div className={isOpen ? `${styles.first} ${styles.firstX}` : styles.first}></div>
+        <div className={isOpen ? `${styles.second} ${styles.secondX}` : styles.second}></div>
+        <div className={isOpen ? `${styles.third} ${styles.thirdX}` : styles.third}></div>
+      </div>
     </div>
   )
 }
