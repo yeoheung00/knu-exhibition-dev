8787d73eca89ecfd2f3d7e03042eee7808acab82
민경원 <min-kyeongwon@mingyeong-won-ui-noteubug.local>
2023-10-16 02:12:27 +0900

    afd

--- components/workItem.tsx
+++ components/workItem.tsx
@@ -1,22 +1,22 @@
-import Link from 'next/link';
-import styles from './workItem.module.css'
-import { Post } from 'contentlayer/generated';
-import graduate from 'db/graduate.json'
-import Image from 'next/image';
+// import Link from 'next/link';
+// import styles from './workItem.module.css'
+// import { Post } from 'contentlayer/generated';
+// import graduate from 'db/graduate.json'
+// import Image from 'next/image';
 
-type PropsType ={
-  url: string;
-  title: string;
-  designer: number;
-}
+// type PropsType ={
+//   url: string;
+//   title: string;
+//   designer: number;
+// }
 
-export default function WorkItem(post: PropsType){
-  const index = post.url.split('/').at(-1);
-  return(
-    <Link className={styles.root} href={post.url}>
-      <Image className={styles.thumb} src={`/profile/${post.designer}/${index}/thumb.jpg`} alt='thumbnail' sizes="100vw" width={300} height={300}/>
-      <h2 className={styles.title}>{post.title}</h2>
-      <h3 className={styles.designer}>{graduate[post.designer].name}</h3>
-    </Link>
-  )
-}
+// export default function WorkItem(post: PropsType){
+//   const index = post.url.split('/').at(-1);
+//   return(
+//     <Link className={styles.root} href={post.url}>
+//       <Image className={styles.thumb} src={`/profile/${post.designer}/${index}/thumb.jpg`} alt='thumbnail' sizes="100vw" width={300} height={300}/>
+//       <h2 className={styles.title}>{post.title}</h2>
+//       <h3 className={styles.designer}>{graduate[post.designer].name}</h3>
+//     </Link>
+//   )
+// }
