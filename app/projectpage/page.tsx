7ea0bd43b5591c6146f63ccb3b705714a3e4fa4d
민경원 <min-kyeongwon@mingyeong-won-ui-noteubug.local>
2023-10-16 00:31:38 +0900

    test3

--- app/projectpage/page.tsx
+++ app/projectpage/page.tsx
@@ -1,11 +1,29 @@
 'use client'
 import { useState } from "react"
-import { allPosts } from 'contentlayer/generated'
+import { Post, allPosts } from 'contentlayer/generated'
 import styles from "./page.module.css"
 import Tab from "components/tab"
 import WorkItem from "@/components/workItem"
+import Link from "next/link"
+
+function PostCard(post: Post) {
+
+  return (
+    <div className="mb-8">
+      <h2 className="m-12 text-4xl text-red-600">
+        <Link
+          href={post.url}
+          className="text-blue-700 hover:text-blue-900"
+          legacyBehavior>
+          {post.title}
+        </Link>
+      </h2>
+    </div>
+  );
+}
 
 export default function Project() {
+  const posts = allPosts.sort();
   //const subject_all = allPosts.map((item) => item.category);
   //const subjectList = subject_all.filter((item, index) => { return subject_all.indexOf(item) === index }).sort();
   const [current, setCurrent] = useState('advertising');
@@ -14,6 +32,9 @@ export default function Project() {
       {/* <Tab setCurrent={setCurrent} list={subjectList} current={current} /> */}
       <div className={styles.listViewer}>
         <div className={styles.listwrap}>
+        {posts.map((post, idx) => (
+        <PostCard key={idx} {...post} />
+      ))}
           {/* {
             allPosts.sort((a, b) => {
               if (a.title > b.title) return 1;
