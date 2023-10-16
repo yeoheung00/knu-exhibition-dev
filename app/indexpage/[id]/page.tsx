2b1f9c7e00dbf01dbb92fe0ec2886eb77ba9fdf3
민경원 <min-kyeongwon@mingyeong-won-ui-noteubug.local>
2023-10-16 14:15:54 +0900

    fed

--- app/indexpage/[id]/page.tsx
+++ app/indexpage/[id]/page.tsx
@@ -1,9 +1,11 @@
-import Worksum from '@/components/worksum';
 import styles from './page.module.css'
 import db from 'db/graduate.json'
+import { allPosts } from 'contentlayer/generated'
+import WorkItem from '@/components/workItem';
 
 export default function Page({ params }: { params: { id: number } }) {
   const { id } = params;
+  const posts = allPosts.filter((item) => item.designer == id ? item : null);
   return (
     <main className={styles.root}>
       <div className={styles.profile}>
@@ -24,6 +26,11 @@ export default function Page({ params }: { params: { id: number } }) {
           </div>
           <p>{db[id].des}</p>
         </div>
+      </div>
+      <div className={styles.worklist}>
+        {
+          posts.map((item, index) => <WorkItem key={index} url={item.url} title={item.title} designer={item.designer} />)
+        }
       </div>
     </main>
   )
